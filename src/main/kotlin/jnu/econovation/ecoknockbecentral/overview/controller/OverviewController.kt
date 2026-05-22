d8e6337bc2a2fbd2719298543a535d581f779167
rdme0 <we6610@gmail.com>
2026-05-23 00:55:21 +0900

    :sparkles: feat: overview shortcut 조회와 수정 API를 추가

---
+++ src/main/kotlin/jnu/econovation/ecoknockbecentral/overview/controller/OverviewController.kt
@@ -0,0 +1,43 @@
+package jnu.econovation.ecoknockbecentral.overview.controller
+
+import jnu.econovation.ecoknockbecentral.common.dto.response.CommonResponse
+import jnu.econovation.ecoknockbecentral.common.dto.response.CommonResponse.emptySuccess
+import jnu.econovation.ecoknockbecentral.common.dto.response.CommonResponse.success
+import jnu.econovation.ecoknockbecentral.common.security.dto.EcoKnockUserDetails
+import jnu.econovation.ecoknockbecentral.overview.dto.request.UpdateOverviewShortcutRequest
+import jnu.econovation.ecoknockbecentral.overview.dto.response.GetShortcutsResponse
+import jnu.econovation.ecoknockbecentral.overview.service.OverviewService
+import org.springframework.http.ResponseEntity
+import org.springframework.http.ResponseEntity.ok
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/overview/shortcuts")
+class OverviewController(private val service: OverviewService) {
+
+    @GetMapping
+    fun getShortcuts(
+        @AuthenticationPrincipal userDetails: EcoKnockUserDetails
+    ): ResponseEntity<CommonResponse<GetShortcutsResponse>> {
+        return ok(success(service.getOverviewShortcuts(memberInfo = userDetails.memberInfo)))
+    }
+
+    @PutMapping
+    fun updateShortcuts(
+        @AuthenticationPrincipal userDetails: EcoKnockUserDetails,
+        @RequestBody request: UpdateOverviewShortcutRequest
+    ): ResponseEntity<CommonResponse<Void>> {
+        service.updateOverviewShortcuts(memberInfo = userDetails.memberInfo, updateRequest = request)
+        return ok(emptySuccess())
+    }
+
+    @PutMapping("/reset")
+    fun resetShortcutsToDefault(
+        @AuthenticationPrincipal userDetails: EcoKnockUserDetails
+    ): ResponseEntity<CommonResponse<Void>> {
+        service.initOverviewShortcuts(memberInfo = userDetails.memberInfo)
+        return ok(emptySuccess())
+    }
+
+}
