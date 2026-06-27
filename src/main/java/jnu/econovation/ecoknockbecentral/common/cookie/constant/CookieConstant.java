5d6575750af798c4b5e133eeb72a9c2bde11a765
rdme0 <we6610@gmail.com>
2026-06-27 16:40:39 +0900

    :recycle: refactor: 인증 쿠키 상수를 auth 패키지로 이동

--- src/main/java/jnu/econovation/ecoknockbecentral/common/cookie/constant/CookieConstant.java
+++
@@ -1,6 +0,0 @@
-package jnu.econovation.ecoknockbecentral.common.cookie.constant;
-
-public final class CookieConstant {
-    public final static String REDIRECT_ORIGIN_KEY = "OAUTH2_REDIRECT_ORIGIN";
-    public final static int OAUTH2_COOKIE_MAX_AGE = 600;
-}
