6fb2d860cc77bd24b6376684a9ca8bb43d685030
rdme0 <we6610@gmail.com>
2026-06-29 17:53:04 +0900

    :sparkles: feat: 관리자 마스터 비밀번호 인증을 추가

---
+++ src/main/java/jnu/econovation/ecoknockbecentral/common/security/constant/AdminAuthConstant.java
@@ -0,0 +1,9 @@
+package jnu.econovation.ecoknockbecentral.common.security.constant;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class AdminAuthConstant {
+    public static final String ADMIN_MASTER_TOKEN = "adminMasterToken";
+}
