171d6e3a851bd7fdd21797c9aa1a35f819766670
rdme0 <we6610@gmail.com>
2026-07-17 21:56:33 +0900

    :sparkles: feat: 게스트 회원 생명주기 처리를 추가

--- src/main/kotlin/jnu/econovation/ecoknockbecentral/member/repository/MemberRepository.kt
+++ src/main/kotlin/jnu/econovation/ecoknockbecentral/member/repository/MemberRepository.kt
@@ -3,8 +3,12 @@ package jnu.econovation.ecoknockbecentral.member.repository
 import jnu.econovation.ecoknockbecentral.member.model.entity.Member
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.time.Instant
+import jnu.econovation.ecoknockbecentral.member.model.vo.Role
 
 @Repository
 interface MemberRepository : JpaRepository<Member, Long> {
     fun findBySsoMemberId(ssoMemberId: Long): Member?
+
+    fun findAllByRoleAndGuestExpiresAtLessThanEqual(role: Role, guestExpiresAt: Instant): List<Member>
 }
