69ba07ae589b6a019af0d971265a84a5197cad2d
junhee-go <junhee.go.io@gmail.com>
2026-07-18 21:39:11 +0900

    ♻️ refactor: 회원 지갑 생성 리스너 로깅 간소화

--- src/main/java/jnu/econovation/ecoknockbecentral/wallet/event/MemberCreatedWalletEventListener.java
+++ src/main/java/jnu/econovation/ecoknockbecentral/wallet/event/MemberCreatedWalletEventListener.java
@@ -2,16 +2,14 @@ package jnu.econovation.ecoknockbecentral.wallet.event;
 
 import jnu.econovation.ecoknockbecentral.member.event.MemberCreatedEvent;
 import jnu.econovation.ecoknockbecentral.wallet.service.MemberWalletService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
+@Slf4j
 @Component
 public class MemberCreatedWalletEventListener {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(MemberCreatedWalletEventListener.class);
 
     private final MemberWalletService memberWalletService;
 
@@ -24,7 +22,7 @@ public class MemberCreatedWalletEventListener {
         try {
             memberWalletService.createManagedWalletIfAbsent(event.getMemberId());
         } catch (Exception exception) {
-            LOGGER.error("Failed to create managed wallet for memberId={}", event.getMemberId(), exception);
+            log.error("Failed to create managed wallet for memberId={}", event.getMemberId(), exception);
         }
     }
 }
