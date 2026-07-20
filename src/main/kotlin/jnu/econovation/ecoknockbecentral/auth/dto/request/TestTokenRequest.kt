fc5167c4c7c4ab077c16a3497944ec2b5ee8fa8f
rdme0 <we6610@gmail.com>
2026-07-20 20:47:14 +0900

    :fire: remove: 테스트 인증과 가상 관리자 토큰 경로를 제거

--- src/main/kotlin/jnu/econovation/ecoknockbecentral/auth/dto/request/TestTokenRequest.kt
+++
@@ -1,8 +0,0 @@
-package jnu.econovation.ecoknockbecentral.auth.dto.request
-
-import io.swagger.v3.oas.annotations.media.Schema
-
-data class TestTokenRequest(
-    @field:Schema(description = "관리자 마스터 비밀번호", example = "master-password")
-    val password: String?,
-)
